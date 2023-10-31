e35cf65772d49c2ea2580639af1b437cdbcb5241
sergio <sergiomc11756@gmail.com>
2023-10-31 21:39:06 +0100

    Modificaciones

--- app/src/main/java/com/iescomercio/menuprincipal/Login.java
+++ app/src/main/java/com/iescomercio/menuprincipal/Login.java
@@ -6,7 +6,7 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class login extends AppCompatActivity {
+public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
