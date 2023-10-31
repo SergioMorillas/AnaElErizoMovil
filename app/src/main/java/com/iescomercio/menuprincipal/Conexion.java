64cdbca9b994985e132701f322cb48be187e1fe6
sergio <sergiomc11756@gmail.com>
2023-10-31 21:39:32 +0100

    Modificaciones

--- app/src/main/java/com/iescomercio/menuprincipal/Conexion.java
+++ app/src/main/java/com/iescomercio/menuprincipal/Conexion.java
@@ -6,7 +6,7 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class conexion extends AppCompatActivity {
+public class Conexion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
