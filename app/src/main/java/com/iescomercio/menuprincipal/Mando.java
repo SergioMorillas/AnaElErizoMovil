e6d93879a899a89ebf0d88272bd1c6f1a643f4ce
ggomezl01 <gonzalogl2004@gmail.com>
2023-11-06 20:51:48 +0100

    Fin del diseño del mando

--- app/src/main/java/com/iescomercio/menuprincipal/Mando.java
+++ app/src/main/java/com/iescomercio/menuprincipal/Mando.java
@@ -1,7 +1,15 @@
 package com.iescomercio.menuprincipal;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Mando extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mando);
+    }
 
 }
