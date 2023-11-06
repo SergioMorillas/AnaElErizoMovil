1935b3529bbe21f3cd1396ef2952e57cca494ef3
sergio <sergiomc11756@gmail.com>
2023-11-06 21:33:28 +0100

    Configuración completa, funciona todo perfecto

--- app/src/main/java/com/iescomercio/menuprincipal/Estadisticas.java
+++ app/src/main/java/com/iescomercio/menuprincipal/Estadisticas.java
@@ -1,6 +1,13 @@
 package com.iescomercio.menuprincipal;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Estadisticas extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.estadisticas);
+    }
 }
--- app/src/main/java/com/iescomercio/menuprincipal/Mando.java
+++ app/src/main/java/com/iescomercio/menuprincipal/Mando.java
@@ -5,11 +5,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Mando extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mando);
     }
-
 }
