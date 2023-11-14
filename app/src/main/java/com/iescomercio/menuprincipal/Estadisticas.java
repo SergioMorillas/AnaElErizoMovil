1ee89f4c3837c6fef4c4a8094adf1f7c3f9902bf
ggomezl01 <gonzalogl2004@gmail.com>
2023-11-14 21:43:44 +0100

    Añadido plugin de estadísticas y mejora de todas las pantallas

--- app/src/main/java/com/iescomercio/menuprincipal/Estadisticas.java
+++ app/src/main/java/com/iescomercio/menuprincipal/Estadisticas.java
@@ -1,6 +1,8 @@
 package com.iescomercio.menuprincipal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,10 @@ public class Estadisticas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.estadisticas);
+    }
+
+    public void lanzarMenu(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
